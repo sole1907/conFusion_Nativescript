9584e1504c2da588452d05cb2a1b643a4a182bb3
ayansola_akanmu <ayansola_akanmu@mastercard.com>
2020-06-04 23:47:21 +0300

    Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { MenuComponent } from "./menu/menu.component";
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
+    { path: "menu", component: MenuComponent },
+    { path: "dishdetail/:id", component: DishdetailComponent },
 ];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+    exports: [NativeScriptRouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,45 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { DishService } from "./services/dish.service";
+import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
+import { baseURL } from "./shared/baseurl";
+import { MenuComponent } from "./menu/menu.component";
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
-// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
+    bootstrap: [AppComponent],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpClientModule,
+        HttpClientModule,
+        NativeScriptUISideDrawerModule,
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent,
+        DishdetailComponent,
+        HomeComponent,
     ],
-    providers: [],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+    providers: [
+        { provide: "baseURL", useValue: baseURL },
+        DishService,
+        ProcessHTTPMsgService,
+    ],
+    schemas: [NO_ERRORS_SCHEMA],
 })
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
-export class AppModule { }
+export class AppModule {}
